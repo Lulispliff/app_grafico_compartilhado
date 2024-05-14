1c2bbf5653a4f6ed71bca67c144da958e5677185
Diogo Luis Colassiol <eduardo@datachamp.com.br>
2024-05-14 11:23:08 -0300

    vaitomanocu

--- lib/src/screens/cotacao_screen.dart
+++ lib/src/screens/cotacao_screen.dart
@@ -1,7 +1,10 @@
+import 'package:app_grafico_compartilhado/src/isar/moeda_database.dart';
+import 'package:app_grafico_compartilhado/src/isar/moeda_model.dart';
 import 'package:app_grafico_compartilhado/src/models/cotacao.dart';
 import 'package:app_grafico_compartilhado/src/screens/grafico_screen.dart';
 import 'package:app_grafico_compartilhado/src/screens/moeda_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CotacaoScreen extends StatefulWidget {
   const CotacaoScreen({super.key});
@@ -68,12 +71,65 @@ class CotacaoScreenState extends State<CotacaoScreen> {
           );
   }
 
+  void addCotacaoDialog() {
+    final moedaDatabase = context.watch<MoedaDatabase>();
+    List<Moeda> currentMoeda = moedaDatabase.currentMoeda;
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Selecione uma moeda'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Selecione uma opção:'),
+                DropdownButton<Moeda>(
+                  items: currentMoeda.map((Moeda moeda) {
+                    return DropdownMenuItem<Moeda>(
+                      value: moeda,
+                      child: Text(moeda.nome),
+                    );
+                  }).toList(),
+                  onChanged: (Moeda? selectedMoeda) {},
+                  hint: const Text('Selecione uma moeda'),
+                ),
+                const SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'asdada',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'dsadsa',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Fechar'),
+              ),
+            ],
+          );
+        });
+  }
+
   Widget _buildAddCotacaoButton() {
     return SizedBox(
       height: 50,
       width: 50,
       child: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          addCotacaoDialog();
+        },
         tooltip: "Adicionar cotação",
         backgroundColor: Colors.blue,
         shape: const CircleBorder(),
