991b8fd2a8c9af8d782201def351e6c6d16ecba3
Diogo Luis Colassiol <eduardo@datachamp.com.br>
2024-05-28 15:06:31 -0300

    lista das moedas

--- lib/src/api/repositories/moeda_repository.dart
+++ lib/src/api/repositories/moeda_repository.dart
@@ -4,7 +4,6 @@ import 'package:app_grafico_compartilhado/src/api/http/exceptions.dart';
 import 'package:app_grafico_compartilhado/src/api/http/http_client.dart';
 import 'package:app_grafico_compartilhado/src/api/models/moeda_api.dart';
 
-
 abstract class IMoedaRepository {
   Future<List<MoedaApi>> getMoedas();
 }
@@ -18,22 +17,23 @@ class MoedaRepository implements IMoedaRepository {
   Future<List<MoedaApi>> getMoedas() async {
     final response = await client.get(
       url:
-          'https://economia.awesomeapi.com.br/USD-BRL/10?start_date=20200301&end_date=20200330',
+          'https://economia.awesomeapi.com.br/USD-BRL/50?start_date=20200301&end_date=20200330',
     );
-    if (response.statusCode == 200){
+    if (response.statusCode == 200) {
       final List<MoedaApi> moedas = [];
 
-      final body  = jsonDecode(response.body);
+      //body recebe os maps que contem os itens
+      final body = jsonDecode(response.body);
 
-       (body as List).map((item) {
+      (body as List).map((item) {
         final MoedaApi moeda = MoedaApi.fromMap(item);
         moedas.add(moeda);
       }).toList();
 
       return moedas;
-    } else if (response.statusCode == 404){
+    } else if (response.statusCode == 404) {
       throw NotFoundException('A url informada não é válida!');
-    }else {
+    } else {
       throw Exception('Não foi possível carregar as moedas!');
     }
   }
