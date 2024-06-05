10a85bfccb78cc217401fc04aed10dc2d906faf4
Luis Poletto <user07@datachamp.local>
2024-06-05 08:04:13 -0300

    mensagens de erro atualizadas

--- lib/utils/error_messages.dart
+++ lib/utils/error_messages.dart
@@ -4,113 +4,151 @@ import 'package:flutter/material.dart';
 
 class ErrorMessages {
   static void cotacaoAddErrorMessage(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        duration: Duration(milliseconds: 1200),
-        backgroundColor: AppColors.color2,
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.info, color: AppColors.color4),
-            SizedBox(width: 8),
-            Text(
-              "Insira os dados corretamente!",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: AppColors.color4,
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          backgroundColor: AppColors.color4,
+          title: Text("Cadastro de cotações", textAlign: TextAlign.center),
+          titleTextStyle: TextStyle(
+              color: AppColors.color2,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.info,
+                color: AppColors.color2,
+                size: 40,
               ),
-            ),
-          ],
-        ),
-      ),
+              Text(
+                "Preencha todos os campos corretamente!",
+                style: TextStyle(
+                    color: AppColors.color1,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 
   static void graficoMoedaErrorMessage(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-        duration: Duration(milliseconds: 1200),
-        backgroundColor: AppColors.color2,
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.info, color: AppColors.color4),
-            SizedBox(width: 8),
-            Text(
-              "Você deve selecionar uma moeda para gerar o gráfico!",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: AppColors.color4),
-            )
-          ],
-        )));
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          backgroundColor: AppColors.color4,
+          title: Text("Gráfico de cotações", textAlign: TextAlign.center),
+          titleTextStyle: TextStyle(
+              color: AppColors.color2,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(Icons.info, color: AppColors.color2, size: 40),
+              Text(
+                "Você deve selecionar uma moeda para gerar o gráfico!",
+                style: TextStyle(
+                    color: AppColors.color1,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 
   static void graficoCotacaoErrorMessage(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      duration: Duration(milliseconds: 1200),
-      backgroundColor: AppColors.color2,
-      content: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.info, color: AppColors.color4),
-          SizedBox(width: 8),
-          Text(
-            "Nenhuma cotação foi encontrada no período de tempo selecionado!",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: AppColors.color4),
-          )
-        ],
-      ),
-    ));
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          backgroundColor: AppColors.color4,
+          title: Text("Gráfico de cotações", textAlign: TextAlign.center),
+          titleTextStyle: TextStyle(
+              color: AppColors.color2,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(Icons.info, color: AppColors.color2, size: 40),
+              Text(
+                "Nenhuma cotação foi encontrada no período de tempo selecionado!",
+                style: TextStyle(
+                    color: AppColors.color1,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 
   static void moedaNomeErrorMessage(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        duration: Duration(milliseconds: 1200),
-        backgroundColor: AppColors.color2,
-        content: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.info, color: AppColors.color4),
-            SizedBox(width: 8),
-            Text(
-              "Insira um nome a moeda!",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: AppColors.color4,
-              ),
-            ),
-          ],
-        ),
-      ),
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          backgroundColor: AppColors.color4,
+          title: Text("Cadastro de moedas", textAlign: TextAlign.center),
+          titleTextStyle: TextStyle(
+              color: AppColors.color2,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(Icons.info, color: AppColors.color2, size: 40),
+              Text(
+                "Insira um nome para a moeda!",
+                style: TextStyle(
+                    color: AppColors.color1,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 
   static void moedaNovoNomeErrorMessage(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      duration: Duration(milliseconds: 1200),
-      backgroundColor: AppColors.color2,
-      content: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.info, color: AppColors.color4),
-          SizedBox(width: 8),
-          Text(
-            "Você deve inserir um novo nome para a moeda que está editando!",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-                color: AppColors.color4),
-          )
-        ],
-      ),
-    ));
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text("Edição de moedas", textAlign: TextAlign.center),
+          titleTextStyle: TextStyle(
+              color: AppColors.color2,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(Icons.info, color: AppColors.color2, size: 40),
+              Text(
+                "Você deve inserir um novo nome para a moeda que está editando!",
+                style: TextStyle(
+                    color: AppColors.color1,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 
   static void errorGraficoNavigatorMessage(BuildContext context) {
