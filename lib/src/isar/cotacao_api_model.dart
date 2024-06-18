b49cdb91ecca322a1de465f3f2c173490027cd3a
Luis Poletto <user07@datachamp.local>
2024-06-18 17:31:35 -0300

    coments

--- lib/src/isar/cotacao_api_model.dart
+++ lib/src/isar/cotacao_api_model.dart
@@ -9,15 +9,15 @@ class CotacoesAPI {
     required this.bid,
   });
 
+  // Recebe um mapa com os dados do JSON e inicializa os campos com esses dados recebidos
   CotacoesAPI.fromJson(Map<String, dynamic> json) {
     name = json.containsKey('name') ? json['name'] : 'N/A';
-    //
     timestamp = DateTime.fromMillisecondsSinceEpoch(
         int.parse(json['timestamp']) * 1000);
-    //
     bid = double.parse(json['bid']);
   }
 
+  //Converte a lista de dados JSON obtida acima para uma lista de objetos "CotacoesAPI"
   static List<CotacoesAPI> fromJsonList(List<dynamic> jsonList) {
     return jsonList.map((json) => CotacoesAPI.fromJson(json)).toList();
   }
