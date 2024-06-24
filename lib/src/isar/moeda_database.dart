0da10fb8dcddbccc69890a918f53d11d1aa5a2ea
Diogo Luis Colassiol <eduardo@datachamp.com.br>
2024-06-24 15:31:33 -0300

    cotações sendo excluidas junto com a exclusão/edição da moeda

--- lib/src/isar/moeda_database.dart
+++ lib/src/isar/moeda_database.dart
@@ -1,3 +1,4 @@
+import 'package:app_grafico_compartilhado/src/isar/cotacao_model.dart';
 import 'package:app_grafico_compartilhado/src/isar/isar_service.dart';
 import 'package:app_grafico_compartilhado/src/isar/moeda_model.dart';
 import 'package:flutter/material.dart';
@@ -35,18 +36,40 @@ class MoedaDatabase extends ChangeNotifier {
 
   // Método para atualizar uma moeda pelo ID
   Future<void> updateMoeda(int id, String newMoeda) async {
-    final existingMoeda = await IsarService.isar.moedas.get(id);
-    if (existingMoeda != null) {
-      existingMoeda.nome = newMoeda;
-      await IsarService.isar
-          .writeTxn(() => IsarService.isar.moedas.put(existingMoeda));
-      await fetchMoedas();
-    }
+    final isar = IsarService.isar;
+    await isar.writeTxn(() async {
+      final existingMoeda = await isar.moedas.get(id);
+      if (existingMoeda != null) {
+        // Deleta todas as cotações associadas à moeda
+        final cotacoes = await isar.cotacoess
+            .filter()
+            .nomeEqualTo(existingMoeda.nome)
+            .findAll();
+        await isar.cotacoess.deleteAll(cotacoes.map((e) => e.id).toList());
+
+        // Atualiza a moeda
+        existingMoeda.nome = newMoeda;
+        await isar.moedas.put(existingMoeda);
+      }
+    });
+    await fetchMoedas();
   }
 
   // Método para deletar uma moeda pelo ID
   Future<void> deleteMoeda(int id) async {
-    await IsarService.isar.writeTxn(() => IsarService.isar.moedas.delete(id));
+    final isar = IsarService.isar;
+    await isar.writeTxn(() async {
+      // Obtém a moeda pelo ID
+      final moeda = await isar.moedas.get(id);
+      if (moeda != null) {
+        // Deleta todas as cotações que têm o mesmo nome da moeda
+        final cotacoes =
+            await isar.cotacoess.filter().nomeEqualTo(moeda.nome).findAll();
+        await isar.cotacoess.deleteAll(cotacoes.map((e) => e.id).toList());
+        // Deleta a moeda
+        await isar.moedas.delete(id);
+      }
+    });
     await fetchMoedas();
   }
 }
